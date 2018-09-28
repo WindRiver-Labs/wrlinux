5cd5b94336ad8f7538b7866662d3d4025a6d4a73
Mark Hatle <mark.hatle@windriver.com>
2018-09-28 11:21:38 -0400

    wrlinux-overc: Enable support for immutable configurations
    Issue: US118189
    
    In order to enforce only supported configurations are allowed, we override
    the existing OEBasicHash class with our own OverCBasicHash.  OverCBasicHash
    is derived from OEBasicHash.
    
    OverCBasicHash will check for undefined items and specifically block them,
    as well as items that have a mismatched hash.
    
    If no issues are found with the additional checking, all behavior falls back
    to standard OEBasicHash behavior.
    
    Note: these items can be disabled by setting:
    
        SIGGEN_OVERC_DEVEL = "1"
    
    Moving into devel mode has a side effect that any specified hashes will be
    completely ignored by both OverCBasicHash and it's parent class OEBasicHash.
    
    It is necessary to set this when generating a new hash file.
    
    Hash files should be generated using:
    
    bitbake -S none <all target recipes>
    
    Using the conf/local.conf and conf/bblayers.conf that directly match the
    expected configuration.
    
    Signed-off-by: Mark Hatle <mark.hatle@windriver.com>

---
+++ wrlinux-overc/lib/overc/immutable.py
@@ -0,0 +1,42 @@
+import bb.siggen
+import oe.sstatesig
+
+class SignatureGeneratorOverCBasicHash(oe.sstatesig.SignatureGeneratorOEBasicHash):
+    name = "OverCBasicHash"
+
+    def init_rundepcheck(self, data):
+        oe.sstatesig.SignatureGeneratorOEBasicHash.init_rundepcheck(self, data)
+
+        self.undefined_msgs = []
+
+        self.dev_mode = data.getVar("SIGGEN_OVERC_DEVEL") or "0"
+
+        if self.dev_mode != "0":
+            # Clear the locked sigs, to avoid contamination in devel mode
+            self.lockedsigs = {}
+
+    def get_taskhash(self, fn, task, deps, dataCache):
+        if self.dev_mode == "0":
+            recipename = dataCache.pkg_fn[fn]
+            if recipename not in self.lockedsigs:
+                self.undefined_msgs.append('The %s:%s task does not have a defined signature.'
+                                      % (recipename, task))
+
+        h = oe.sstatesig.SignatureGeneratorOEBasicHash.get_taskhash(self, fn, task, deps, dataCache)
+        return h
+
+    def checkhashes(self, missed, ret, sq_fn, sq_task, sq_hash, sq_hashfn, d):
+        if self.mismatch_msgs or self.undefined_msgs:
+            if self.mismatch_msgs:
+                bb.error("\n".join(self.mismatch_msgs))
+            if self.undefined_msgs:
+                bb.error("\n".join(self.undefined_msgs))
+            bb.fatal("Mismatched signatures or undefined signatures indicate "
+                     "a possibly unsupported configuration. Only tested "
+                     "configurations are supported. Please follow the "
+                     "configuration steps exactly. If you believe this "
+                     "message is in error, please open a support ticket.")
+
+        oe.sstatesig.SignatureGeneratorOEBasicHash.checkhashes(self, missed, ret, sq_fn, sq_task, sq_hash, sq_hashfn, d)
+
+bb.siggen.SignatureGeneratorOverCBasicHash = SignatureGeneratorOverCBasicHash
