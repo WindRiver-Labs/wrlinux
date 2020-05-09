ea6af6e23016324bfcf235bbcbfef1ea613756fd
Hongxu Jia <hongxu.jia@windriver.com>
2020-05-09 10:15:49 +0800

    add recipe nativesdk-wic
    Add nativesdk-wic, which adds Yocto build system poky (oe-core and
    bitbake) to nativesdk.
    
    Issue: LINUXEXEC-7226
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

---
+++ recipes-support/meta/files/wic_wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+builddir=`mktemp -d ./buildXXXX`
+builddir=`realpath $builddir`
+OE_SKIP_SDK_CHECK=1
+FAKEROOTCMD="$OECORE_NATIVE_SYSROOT/usr/bin/pseudo"
+
+. $OECORE_NATIVE_SYSROOT/usr/share/poky/oe-init-build-env $builddir >/dev/null
+cd -
+
+case $1 in
+    create)
+        shift
+        FAKEROOTCMD=${FAKEROOTCMD} wic create -n $OECORE_NATIVE_SYSROOT -s -b $OECORE_TARGET_SYSROOT/usr/share $@
+        ;;
+    *)
+        wic $@
+        ;;
+esac
+
+rm -rf $builddir
