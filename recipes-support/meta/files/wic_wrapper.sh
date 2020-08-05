f39178934fbb48c5b913fb9865f64a72c00a6f46
Hongxu Jia <hongxu.jia@windriver.com>
2020-08-05 15:31:22 +0800

    nativesdk-wic: exit 1 if call wic failed
    The bootfs.sh use ${PIPESTATUS[0]} to check wic status, but the wic
    wrapper broke it.
    
    Make wrapper exit 1 if wic failed
    
    Issue: LINUXEXEC-7219
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- recipes-support/meta/files/wic_wrapper.sh
+++ recipes-support/meta/files/wic_wrapper.sh
@@ -11,9 +11,17 @@ case $1 in
     create)
         shift
         FAKEROOTCMD=${FAKEROOTCMD} wic create -n $OECORE_NATIVE_SYSROOT -s $@
+        if [ "${PIPESTATUS[0]}" -ne "0" ]; then
+            rm -rf $builddir
+            exit 1
+        fi
         ;;
     *)
         wic $@
+        if [ "${PIPESTATUS[0]}" -ne "0" ]; then
+            rm -rf $builddir
+            exit 1
+        fi
         ;;
 esac
 
